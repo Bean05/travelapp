eda9fae66aad99f7f98ce2afda5167e638b3621f
Kristina <kotova.kristina@gmail.com>
2022-09-29 18:26:32 +0300

    front in & up

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import Button from '@mui/material/Button';
 import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import LogoutIcon from '@mui/icons-material/Logout';
 import { logoutUserAsync } from '../../redux/actions/userActions';
 
 function Navbar() {
@@ -17,21 +17,27 @@ function Navbar() {
       <AppBar position="static">
         <Toolbar variant="dense">
           <IconButton href="/" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <AccountBalanceIcon />
+            Friends Trip
           </IconButton>
 
           <Button component={NavLink} to="/signup" variant="filled">
-            SignUp
+            Зарегистрироваться
           </Button>
           <Button component={NavLink} to="/signin" variant="filled">
-            SignIn
+            Войти
           </Button>
-          <Button component={NavLink} to="/home" variant="filled">
-            Home
+          <Button component={NavLink} to="/" variant="filled">
+            Создать поездку
           </Button>
-          <Button onClick={() => dispatch(logoutUserAsync())} variant="filled">Logout</Button>
+          <Button component={NavLink} to="/" variant="filled">
+            Найти поездку
+          </Button>
 
-
+          {/* <Button onClick={() => dispatch(logoutUserAsync())}
+          variant="filled">Выйти</Button> */}
+          <IconButton onClick={() => dispatch(logoutUserAsync())} edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <LogoutIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,7 +1,137 @@
 import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useDispatch } from 'react-redux';
+import { signupUser } from '../../redux/actions/userActions';
+
+const theme = createTheme();
 
 export default function SignUp() {
+  const dispatch = useDispatch();
   return (
-    <div>SignUp</div>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Регистрация
+            </Typography>
+            <Box component="form" noValidate onSubmit={(e) => dispatch(signupUser(e, Object.fromEntries(new FormData(e.target))))} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Имя"
+                name="name"
+                autoComplete="name"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Почта"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Пароль"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="age"
+                label="Возраст"
+                name="age"
+                autoComplete="age"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="phone"
+                label="Телефон"
+                name="phone"
+                autoComplete="phone"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="social"
+                label="Соц.Сеть"
+                name="social"
+                autoComplete="social"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="photo"
+                label="Фото"
+                name="photo"
+                autoComplete="photo"
+                autoFocus
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
