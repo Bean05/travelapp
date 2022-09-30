303e0a480a1afbeda32d1c73d7072d2f8fb8313d
Andrey K <coke@list.ru>
2022-09-30 10:04:19 +0300

    save1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
+import { Container } from '@mui/material';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+// import Navbar from './components/Navbar';
 import Search from './components/Search';
-import SignIn from './components/UserAccount/SignIn';
-import SignUp from './components/UserAccount/SignUp';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <Container>
-    <Navbar />
-    <Routes>
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/search" element={<Search />} />
-    </Routes>
-  </Container>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Container>
   );
 }
 
--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { TextField } from '@mui/material';
+import React from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import './search.css';
 
 export default function Search() {
+  const [value, setValue] = React.useState(null);
   return (
-    <div>Search</div>
-  )
+    <div className="searchFatherDiv">
+      <div className="indexDiv">
+        <LocalizationProvider dateAdapter={AdapterDayjs} />
+        <DatePicker
+          label="Basic example"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </div>
+      <div className="indexDiv">
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </div>
+      <div className="indexDiv">
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </div>
+    </div>
+  );
 }
