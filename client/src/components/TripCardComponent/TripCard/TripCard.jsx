46837f94c3e240709bf602fdf81fb5d25f5dcf48
Nona <rkholmatova@gmail.com>
2022-09-30 21:31:01 +0300

    add trip cards

---
+++ client/src/components/TripCardComponent/TripCard/TripCard.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import TripCardModal from '../TripCardModal';
+import './tripCardStyle.css';
+
+export default function TripCard() {
+  const allCards = useSelector((state) => state.tripCard);
+
+  // console.log(allCards);
+  return (
+    <div className="containerCard">
+      {allCards && allCards?.map((el) => (
+        <div key={el.id}>
+          <div className="box">
+            <div className="card">
+              <div className="card-header">
+                <img src={el.tripPhoto} alt={el.tripName} />
+              </div>
+              <div className="card-body">
+                {/* <span className="tag tag-teal">{el.tripName}</span> */}
+                <h4>
+                  {el.tripName}
+                </h4>
+                <div>
+                  Даты:
+                  {' '}
+                  {el.date}
+                </div>
+                <div>
+                  Откуда:
+                  {' '}
+                  {el.cityStart}
+                </div>
+                <div>
+                  Куда:
+                  {' '}
+                  {el.cityWhere}
+                </div>
+                <div className="user">
+                  <img src={el?.User.photo} alt={el.User.name} />
+                  <div className="user-info">
+                    Организатор
+                    <Link to="/page">
+                      <h5>{el.User.name}</h5>
+                    </Link>
+                  </div>
+                </div>
+                <TripCardModal oneCard={el} />
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- server/routes/tripCardApi.js
+++ server/routes/tripCardApi.js
@@ -1,6 +1,12 @@
 const express = require('express');
-// const { Trip } = require('../db/models');
+const { Trip, User } = require('../db/models');
 
 const router = express.Router();
+
+router.get('/allcards', async (req, res) => {
+  const allCards = await Trip.findAll({ include: User });
+  //   console.log('SERVER', allCards);
+  res.json(allCards);
+});
 
 module.exports = router;
